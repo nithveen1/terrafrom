8ba1adddeb20e40c81ac73eacb8a51d20aa7ae95
nithveen1 <107979233+nithveen1@users.noreply.github.com>
2024-03-26 16:33:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,22 @@ provider "aws" {
     region = "us-east-1"  
 }
 
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+    vpc_id                  = aws_vpc.my_vpc.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "us-east-1a"
+
+}
+
 resource "aws_instance" "foo" {
-  ami           = "ami-0c101f26f147fa7fd" 
-  instance_type = "t2.micro"
-  tags = {
-      Name = "Instance"
-  }
+    ami             = "ami-0c101f26f147fa7fd"
+    instance_type   = "t2.micro"
+    subnet_id       = aws_subnet.my_subnet.id
+    tags = {
+        Name = "TF-Instance"
+    }
 }
